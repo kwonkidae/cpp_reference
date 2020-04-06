71465e10e084d0dac5a675bf4515952f38c8f28b
kwon <kkdosk@naver.com>
2020-04-06 15:00:42 +0900

    iterator_traint iterator_category

---
+++ trait/iterator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <iterator>
+
+template <class BDIter>
+void alg(BDIter, BDIter, std::bidirectional_iterator_tag)
+{
+  std::cout << "alg() called for bidirectional iterator\n";
+}
+
+template <class RAIter>
+void alg(RAIter, RAIter, std::random_access_iterator_tag)
+{
+  std::cout << "alg() called for random-access iterator\n";
+}
+
+template <class Iter>
+void alg(Iter first, Iter last)
+{
+  alg(first, last, typename std::iterator_traits<Iter>::iterator_category());
+}
+
+int main()
+{
+  std::vector<int> v;
+  alg(v.begin(), v.end());
+
+  std::list<int> l;
+  alg(l.begin(), l.end());
+}
